7d6fdc7a94a79a52bbf88cb5bb7cb177519eac88
NashidForbes <NashidForbes@biohumans.com>
2023-09-28 23:53:51 -0400

    ApiGateway enabled configuration for Spring Cloud Config server and enabled dynamic variable in RestTemplate.java

--- src/main/java/com/appsdeveloperblog/estore/apigateway/rest/RestTemplate.java
+++ src/main/java/com/appsdeveloperblog/estore/apigateway/rest/RestTemplate.java
@@ -1,15 +1,19 @@
 package com.appsdeveloperblog.estore.apigateway.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RestTemplate {
+    @Value("${server.port}")
+    private int serverPort;
+
     @Bean
     public org.springframework.web.client.RestTemplate myRestTemplate() {
         return new RestTemplateBuilder()
-                .rootUri("http://localhost:8082")
+                .rootUri("http://localhost:" + serverPort)
                 .build();
     }
 }
